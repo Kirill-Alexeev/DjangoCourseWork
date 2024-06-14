24a957a91a813f5be3a3456308e2d0465b3a25b8
Kirill-Alexeev <kirushkalexeev@gmail.com>
2024-06-14 18:52:13 +0300

    Started make view

--- sweetshop/sweetshop/catalog/urls.py
+++ sweetshop/sweetshop/catalog/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
-    
+    re_path(r"^$", views.index, name="index"),
+    re_path(r"^cakes/$", views.CakeListView.as_view(), name="cakes"),
+    re_path(r"^cake/(?P<pk>\d+)$", views.CakeDetailView.as_view(), name="cake-detail"),
+    re_path(r"^reviews/$", views.ReviewListView.as_view(), name="reviews"),
+    re_path(r"^review/(?P<pk>\d+)$", views.ReviewDetailView.as_view(), name="review-detail"),
 ]
--- sweetshop/sweetshop/catalog/views.py
+++ sweetshop/sweetshop/catalog/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from .models import Cake, Ingredient, Image, Order, Review, Client
+from django.views import generic
 
 # Create your views here.
+
+def index(request):
+    """Функция отображения для домашней страницы сайта."""
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        "index.html",
+        context={},
+    )
+
+class CakeListView(generic.ListView):
+    model = Cake
+    paginate_by = 9
+
+
+class CakeDetailView(generic.DetailView):
+    model = Cake
+
+
+class ReviewListView(generic.ListView):
+    model = Review
+    paginate_by = 9
+
+
+class ReviewDetailView(generic.DetailView):
+    model = Review
 
